eda30939daf9c49d9bc38608be083e89f57f63aa
Saparin Oleg <SaparinOleg@gmail.com>
2023-07-11 23:48:08 +0300

    Add Lesson4

---
+++ Lesson4_24.06.23/Lesson4 Task1. CodeWars - Square(n) Sum.py
@@ -0,0 +1,6 @@
+# https://www.codewars.com/kata/515e271a311df0350d00000f
+
+# Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+numbers = [1, 2, 4, 5, 10]
+print(sum([numb ** 2 for numb in numbers]))
