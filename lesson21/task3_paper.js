a3e222a8dd15cd5a1e51040655378f1eacf63888
Saparin <SaparinOleg@gmail.com>
2023-09-16 06:30:53 +0300

    Fix task3

--- lesson21/task3_paper.js
+++ lesson21/task3_paper.js
@@ -1,5 +1,9 @@
 const sheetsInReamPaper = 500
 const consumptionPerWeek = 1200
 const weeksAmount = 8
-let totalConsumption = consumptionPerWeek * weeksAmount
-console.log((totalConsumption - totalConsumption % sheetsInReamPaper) / sheetsInReamPaper)
+
+let totalAmountOfSheets = consumptionPerWeek * weeksAmount
+let result = 0
+for (; totalAmountOfSheets > 0; totalAmountOfSheets -= sheetsInReamPaper, result++){}
+
+console.log(result)
